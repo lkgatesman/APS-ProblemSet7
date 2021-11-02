edb41c25b2fef86d2b62de81d86ecd4204df0f77
Lauren Gatesman <55667325+lkgatesman@users.noreply.github.com>
2021-11-02 17:51:36 -0400

    Update PoppisRocket.java

--- PoppisRocket.java
+++ PoppisRocket.java
@@ -2,9 +2,7 @@ import java.util.*;
 import java.io.*;
 
 /*
-This is a bottom-up dynamic programming approach to the problem
-Poppi's Rocket from Algorithmic Problem Solving Problem Set #7.
-This solution yields 20.0/20.0 on the GradeScope autograder.
+This is a bottom-up dynamic programming approach.
 */
 
 public class PoppisRocket {
@@ -66,50 +64,4 @@ public class PoppisRocket {
     
 }
 
-/*
-------------
-Problem Statement:
 
-Poppi’s Rocket
-The World Tree is a towering tree standing in the center of the endless Cloud Sea. The fabled paradise Elysium is said to
-be located at the top of the World Tree. The exterior of the World Tree is covered in thick growths of branches, while the
-interior is a high-tech ladder with n rungs.
-
-Poppi can use rocket jumping to climb the ladder, but she cannot
-skip any rungs. Initially Poppi is on the ground. Initial strength of
-the rocket is set to k. As long as her jumps are strictly smaller
-than k, her rocket’s strength remains at k. If her jump is ever
-equal to k, the strength decreases by 1. The rocket cannot be
-used to propel her to the height greater than k.
-
-For example let the height of the rungs from the ground be 2, 7, 8,
-12, 14 respectively and the initial strength of the rocket be k = 5.
-Her jumps are:
-1. Jumped 2 feet from the ground to the 1st rung (ground to 2),
-k remains 5.
-2. Jumped 5 feet to the next rung (2 to 7). So, k decreases to 4.
-3. Jump 1 feet to the 3rd rung (7 to 8). So, k remains 4.
-4. Jump 4 feet to the 4th rung (8 to 12). So, k decreases to 3.
-5. Jump 2 feet to the 5th rung (12 to 14). So, k remains 3.
-
-Since the rockets with the greater strength cost more, Poppi asks
-you for help in figuring out the minimum initial strength k for the
-rocket so that she can reach the top rung.
-
-Input
-The first line contains 1 integer: a positive n (0 < n ≤ 10^5) giving the number of rungs in the ladder.
-The next line contains n space separated integers, r1, r2, … , rn (1 ≤ r1 < r2 < … < rn ≤ 10^7),
-denoting the heights of the rungs from the ground.
-
-Output
-Print the minimum value of k as described above. The output ends with a newline.
-
-Example 1
-Input:
-5
-2 7 8 12 14
-Output:
-5
-
-------------
-*/
